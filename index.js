06253b189431ef6da494d9dad074b8cc2c3b263d
Brian Wangila <bwanjalah@gmail.com>
2022-06-17 01:30:25 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const lowercase = word.toLowerCase();
+  const reversedWord = lowercase.split("").reverse().join("");
+  if (lowercase === reversedWord) {
+    return true;
+  }else{
+    return false;
+  }
 }
+
 
 /* 
   Add your pseudocode here
+  Input: a string
+  Output: boolean
+  Steps to solve the problem:
+  1. convert the string to lowercase
+  2. reverse the string
+  3. compare the reversed string to the original string
 */
 
 /*
   Add written explanation of your solution here
+  I used the reverse method to reverse the string.
+
 */
 
 // You can run `node index.js` to view these console logs
